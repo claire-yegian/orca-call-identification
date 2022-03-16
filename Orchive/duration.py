da483d579d4d77171ff52d76c3459ad255d45470
Claire Yegian <claireyegian@gmail.com>
2022-03-15 18:25:03 -0700

    working on loopy solution, need to figure out wav file indexing

--- Orchive/duration.py
+++ Orchive/duration.py
@@ -8,7 +8,7 @@ max_dur = 0
 for filename in os.listdir(directory):
     if filename.endswith(".wav"):
         name = directory + filename
-        with contextlib.closing(wave.open(name,'r')) as f:
+        with contextlib.closing(wave.open(name, 'r')) as f:
             frames = f.getnframes()
             rate = f.getframerate()
             duration = frames / float(rate)
@@ -21,13 +21,13 @@ print("audible")
 print("min: ", min_dur)
 print("max: ", max_dur)
 
-directory = './../call-catalog/wav/'
+directory = './newVocals/'
 min_dur = 10
 max_dur = 0
 for filename in os.listdir(directory):
     if filename.endswith(".wav"):
         name = directory + filename
-        with contextlib.closing(wave.open(name,'r')) as f:
+        with contextlib.closing(wave.open(name, 'r')) as f:
             frames = f.getnframes()
             rate = f.getframerate()
             duration = frames / float(rate)
@@ -37,29 +37,29 @@ for filename in os.listdir(directory):
                 max_dur = duration
 
 print()
-print("call-catalog")
+print("newVocals")
 print("min: ", min_dur)
 print("max: ", max_dur)
 
-directory = 'extract'
-min_dur = 10
-max_dur = 0
-for filename in os.listdir(directory):
-    if filename.endswith(".wav"):
-        name = 'extract/' + filename
-        with contextlib.closing(wave.open(name,'r')) as f:
-            frames = f.getnframes()
-            rate = f.getframerate()
-            duration = frames / float(rate)
-            if duration < min_dur:
-                min_dur = duration
-            if duration > max_dur:
-                max_dur = duration
+# directory = 'extract'
+# min_dur = 10
+# max_dur = 0
+# for filename in os.listdir(directory):
+#     if filename.endswith(".wav"):
+#         name = 'extract/' + filename
+#         with contextlib.closing(wave.open(name,'r')) as f:
+#             frames = f.getnframes()
+#             rate = f.getframerate()
+#             duration = frames / float(rate)
+#             if duration < min_dur:
+#                 min_dur = duration
+#             if duration > max_dur:
+#                 max_dur = duration
 
-print()
-print("extract")
-print("min: ", min_dur)
-print("max: ", max_dur)
+# print()
+# print("extract")
+# print("min: ", min_dur)
+# print("max: ", max_dur)
 
 
 directory = 'vocals'
@@ -68,7 +68,7 @@ max_dur = 0
 for filename in os.listdir(directory):
     if filename.endswith(".wav"):
         name = 'vocals/' + filename
-        with contextlib.closing(wave.open(name,'r')) as f:
+        with contextlib.closing(wave.open(name, 'r')) as f:
             frames = f.getnframes()
             rate = f.getframerate()
             duration = frames / float(rate)
@@ -81,5 +81,3 @@ print()
 print("vocals")
 print("min: ", min_dur)
 print("max: ", max_dur)
-
-        
